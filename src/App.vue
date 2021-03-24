165db741d15eed2cba3d15eccbe3d9d5725afa11
Mircea Bereveanu <mircea.bereveanu@gmail.com>
2021-03-25 00:23:27 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Yours Vue 2222222222.js App"/>
   </div>
 </template>
 
